3a963dc238bbec36154fed60c35c733c92b5db35
Tom Dent <tomdent991@hotmail.com>
2023-11-02 11:53:53 +0100

    completed challenge

---
+++ js-tip-calculator/index.js
@@ -0,0 +1,14 @@
+let mealCost = 12.99;
+let tipPercentage = 0.1;
+
+let tipAmount = mealCost * tipPercentage;
+
+console.log(tipAmount);
+
+let totalCost = mealCost + tipAmount;
+
+console.log(totalCost);
+
+const finalCost = totalCost.toFixed(2);
+
+console.log(finalCost);
