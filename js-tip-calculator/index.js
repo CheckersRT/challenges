9f11a8198e4c567d14df216f71de7c7c9a95665b
Tom Dent <tomdent991@hotmail.com>
2023-11-03 16:57:15 +0100

    playing around with js

--- js-tip-calculator/index.js
+++ js-tip-calculator/index.js
@@ -12,3 +12,23 @@ console.log(totalCost);
 const finalCost = totalCost.toFixed(2);
 
 console.log(finalCost);
+
+console.log({});
+console.log([]);
+console.log(x => x *2);
+
+console.log(typeof (x => x * 2));
+
+let wine = 0.5
+let beer = 1.5
+
+let guests = 120
+
+function amountOfWine(guests, drink) {
+    result = guests * drink;
+    console.log(result);
+}
+
+drink = wine;
+
+console.log(amountOfWine);
