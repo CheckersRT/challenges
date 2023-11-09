05d963af3e2b3b94e620870dc8ff4037428b32c4
Tom Dent <tomdent991@hotmail.com>
2023-11-09 11:18:15 +0100

    task complete

--- js-inputs-and-strings_switch-values/js/index.js
+++ js-inputs-and-strings_switch-values/js/index.js
@@ -16,11 +16,24 @@ Hint: the HTML entity for the switch button is: &updownarrow
 const firstInput = document.querySelector('[data-js="first-input"]');
 const uppercaseButton = document.querySelector('[data-js="button-uppercase"]');
 const lowercaseButton = document.querySelector('[data-js="button-lowercase"]');
+const switchButton = document.querySelector('[data-js="button-switch"]');
+
+const secondInput = document.querySelector('[data-js="second-input"]')
 
 uppercaseButton.addEventListener("click", () => {
   firstInput.value = firstInput.value.toUpperCase();
+  secondInput.value = secondInput.value.toUpperCase();
 });
 
 lowercaseButton.addEventListener("click", () => {
   firstInput.value = firstInput.value.toLowerCase();
+  secondInput.value = secondInput.value.toLowerCase();
 });
+
+switchButton.addEventListener("click", () => {
+  const originalOne = firstInput.value
+  const originalTwo = secondInput.value;
+  firstInput.value = originalTwo;
+  secondInput.value = originalOne; 
+  // secondInput.value = firstInput.value;
+})
