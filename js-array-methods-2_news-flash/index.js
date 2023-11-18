cbd83195bd546670e4214d618e7946278e58738b
Tom Dent <tomdent991@hotmail.com>
2023-11-18 09:49:59 +0100

    task complete

--- js-array-methods-2_news-flash/index.js
+++ js-array-methods-2_news-flash/index.js
@@ -5,12 +5,20 @@ import { checkFilteredNews, checkSortedNews } from "./utils/results.js";
 const container = document.querySelector('[data-js="card-container"]');
 
 // Part 1 - start here
-const filteredNews = news.filter(() => {
-  return true;
-});
+const filteredNews = news.filter((card) => card.categories.includes("politics"));
+console.log(filteredNews);
 
 // Part 2 - start here
-const sortedNews = filteredNews;
+const sortedNews = filteredNews.slice().sort((a, b) => {
+  const bodyA = a.body.length;
+  const bodyB = b.body.length;
+
+  if(bodyA > bodyB) {
+    return 1;
+  } else if (bodyA < bodyB) {
+    return -1;
+  } return 0;
+})
 
 sortedNews.forEach((news) => {
   const cardElement = Card(news);
