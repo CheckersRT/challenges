c4d6028261d65f6a58635cc2612fc291273a6cf0
Tom Dent <tomdent991@hotmail.com>
2023-11-21 11:04:31 +0100

    task complete

--- js-fetch_star-wars-console/js/index.js
+++ js-fetch_star-wars-console/js/index.js
@@ -1,7 +1,37 @@
 console.clear();
 
-const url = "https://swapi.dev/api/people";
+const url = "https://swapi.dev/api/peopl";
 
-function fetchData() {}
+async function fetchData() {
+
+    try {
+    const response = await fetch(url);
+    console.log(response);
+
+    if(!response.ok) {
+        console.log("oh no!")
+        return
+    }
+
+    const data = await response.json();
+    console.log(data);
+
+    const characterArray = data.results;
+    const characterNames = characterArray.map((character) => character.name);
+    console.log(characterNames);
+
+    const charactersSmallerThan150cm = characterArray.filter((character) => character.height < 150).map((character) => {
+        return {name: character.name, height: character.height} })
+
+
+    console.log(charactersSmallerThan150cm);
+
+} catch(error) {
+    console.error(error);
+
+}
+    
+
+}
 
 fetchData();
