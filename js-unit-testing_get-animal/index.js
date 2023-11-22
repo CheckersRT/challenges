5499e806acb733d45c3fda6282ec03fbb2400d49
Tom Dent <tomdent991@hotmail.com>
2023-11-22 11:10:23 +0100

    task complete

--- js-unit-testing_get-animal/index.js
+++ js-unit-testing_get-animal/index.js
@@ -1,1 +1,9 @@
-export function getAnimal() {}
+export function getAnimal(name) {
+    if(name === "cats") {
+        return "I totally love cats!"
+    } else if(name) {
+        return `I like ${name}!`
+    } else {
+        return "I do not like animals at all!"
+    }
+}
