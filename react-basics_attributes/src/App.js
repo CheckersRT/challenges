9833620120802962e512726b8f0652967a1d3176
Tom Dent <tomdent991@hotmail.com>
2023-11-27 11:39:50 +0100

    task complete

--- react-basics_attributes/src/App.js
+++ react-basics_attributes/src/App.js
@@ -2,5 +2,14 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with an article</div>;
+  return <ArticleComponent/>;
+}
+
+function ArticleComponent() {
+  return <article className="article">
+    <h2 className="aritcle__title">What&apos;s the funniest word in the English language?</h2>
+    <label htmlFor="input"></label>
+    <input id="input"></input>
+    <a className="article__link" href="https://www.ef.com/wwen/blog/language/funniest-words-in-english/">Find out here!</a>
+  </article>
 }
