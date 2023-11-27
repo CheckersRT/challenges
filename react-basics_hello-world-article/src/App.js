04980657c23dd45d311f861dd77a30de04238f6c
Tom Dent <tomdent991@hotmail.com>
2023-11-27 11:28:39 +0100

    task complete

--- react-basics_hello-world-article/src/App.js
+++ react-basics_hello-world-article/src/App.js
@@ -2,5 +2,13 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with your Component!</div>;
+  return <HelloWorldArticle/>;
+}
+
+function HelloWorldArticle() {
+  return <article>
+    <h1>What actually are buttons?</h1>
+    <p>Nobody really knows.</p>
+    <button>Click me, I&apos;m a button</button>
+  </article>
 }
