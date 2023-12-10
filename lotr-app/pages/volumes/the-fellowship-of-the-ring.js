419caeb28f3b5b28774956c8179b610299f42f5b
Tom Dent <tomdent991@hotmail.com>
2023-12-10 22:38:16 +0100

    task complete

--- lotr-app/pages/volumes/the-fellowship-of-the-ring.js
+++ lotr-app/pages/volumes/the-fellowship-of-the-ring.js
@@ -1,13 +1,34 @@
-import volumes from "@/lib/data";
+import { volumes } from "@/lib/data"
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Fellowship() {
-
-    console.log(volumes[0].slug)
-    // volumes.find((volume) => {
-    //     volume.slug === "the-fellowship-of-the-ring";
-    // })
-    
+    const volume = volumes.find((volume) => {
+        return volume.slug === "the-fellowship-of-the-ring";
+    })
+    const currentIndex = volumes.findIndex((volume) => volume.slug === "the-fellowship-of-the-ring");
+    const currentSlug = volumes[currentIndex].slug
+    // console.log(currentSlug)
     return <>
-        <h2>{volumes[0].title}</h2>
+        <h2>{volume.title}</h2>
+        <p>{volume.description}</p>
+        <ul>
+            {volume.books.map((book) => {
+            return <li key={book.ordinal}>{`${book.ordinal}: ${book.title}`}</li>
+            })}
+        </ul>
+        <Image
+            src="/../public/images/the-fellowship-of-the-ring.png"
+            height={230}
+            width={140}
+            alt="cover photo"
+        />
+        <button>
+            <Link href={currentIndex !== 0 ? 
+                `/volumes/${volumes[currentIndex - 1].slug}` :
+                ""
+                }>Previous</Link>
+        </button>
+        <button><Link href={`/volumes/${volumes[currentIndex + 1].slug}`}>Next</Link></button>
     </>
 }
