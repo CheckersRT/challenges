84f6fdbe8db2847ad3eb8d98f4221ea57120d789
Tom Dent <tomdent991@hotmail.com>
2023-12-31 09:07:22 +0100

    latest

--- react-effects-and-fetch_pokemon-api/src/components/PokemonList.js
+++ react-effects-and-fetch_pokemon-api/src/components/PokemonList.js
@@ -1,28 +1,45 @@
 import { useState, useEffect } from "react";
+import vision from "@google-cloud/vision"
+
 
 export default function PokemonList() {
-  const [pokemon, setPokemon] = useState([]);
-
+  const [answer, setAnswer] = useState([]);
+  let extractedString = "";
+  
+  
   useEffect(() => {
-    async function loadPokemon() {
+    let extractedText = [];
+    async function getTextFromImage() {
       try {
-        const response = await fetch("https://pokeapi.co/api/v2/pokemon");
-        const data = await response.json();
-        setPokemon(data.results);
+          const client = new vision.ImageAnnotatorClient();
+      
+          const fileName = "public/IMG_7482.jpg";
+      
+          const [result] = await client.textDetection(fileName);
+          const detections = result.textAnnotations;
+          console.log(detections);
+          detections.forEach((text) => {
+          extractedText.push(text.description)
+          });
+          extractedString = extractedText.toString()
+          console.log(extractedString)
+          setAnswer(extractedString);
       } catch (error) {
         console.log(error);
       }
     }
-    loadPokemon();
+    getTextFromImage();
   }, []);
 
   return (
     <main>
-      <ul>
+      <p>TEst</p>
+      <p>{answer}</p>
+      {/* <ul>
         {pokemon.map(({ name }) => (
           <li key={name}>{name}</li>
         ))}
-      </ul>
+      </ul> */}
     </main>
   );
 }
